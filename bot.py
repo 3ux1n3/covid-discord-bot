2f9ab52b82b4f7d30833ed657d521b0ebba80670
3ux1n3 <3ux1n3@protonmail.com>
2020-04-13 19:52:20 +0700

    rixhard

--- bot.py
+++ bot.py
@@ -24,16 +24,22 @@ async def on_message(message):
     if message.content.lower().startswith('!covid '):
         country = message.content.lower().replace('!covid ', '')
         try:
-            rsp = requests.get("https://corona.lmao.ninja/countries/" + country)
-            # timestamp = rsp.json()["updated"]
-            # updated = datetime.fromtimestamp(timestamp)
-            embed = discord.Embed(title="COVID-19 Stats for " + str(rsp.json()["country"]))
-            embed.set_thumbnail(url=rsp.json()["countryInfo"]["flag"])
-            embed.add_field(name="Total Cases", value=str(rsp.json()["cases"]), inline=False)
-            embed.add_field(name="Total Deaths", value=str(rsp.json()["deaths"]), inline=False)
-            embed.add_field(name="Total Recovered", value=str(rsp.json()["recovered"]), inline=False)
-            # embed.add_field(name="Last Update", value=updated, inline=False)
-            await message.channel.send(embed=embed)
+            if country == "western sahara":
+                embed = discord.Embed(title="SE7RA MGHRIBIA ")
+                embed.set_thumbnail(url="https://i.ytimg.com/vi/LmgSMNqhMXQ/hqdefault.jpg")
+                # embed.add_field(name="Last Update", value=updated, inline=False)
+                await message.channel.send(embed=embed)
+            else:
+                rsp = requests.get("https://corona.lmao.ninja/countries/" + country)
+                # timestamp = rsp.json()["updated"]
+                # updated = datetime.fromtimestamp(timestamp)
+                embed = discord.Embed(title="COVID-19 Stats for " + str(rsp.json()["country"]))
+                embed.set_thumbnail(url=rsp.json()["countryInfo"]["flag"])
+                embed.add_field(name="Total Cases", value=str(rsp.json()["cases"]), inline=False)
+                embed.add_field(name="Total Deaths", value=str(rsp.json()["deaths"]), inline=False)
+                embed.add_field(name="Total Recovered", value=str(rsp.json()["recovered"]), inline=False)
+                # embed.add_field(name="Last Update", value=updated, inline=False)
+                await message.channel.send(embed=embed)
         except Exception as e:
             print(repr(e))
             await message.channel.send("something went wrong maybe country name typo")
